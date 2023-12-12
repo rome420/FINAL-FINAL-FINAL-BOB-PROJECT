90af060f906ed0f95b1b03e42aa8eccb7e7566b9
ildomota <ildo.mota@hotmail.com>
2023-12-12 15:20:03 +0100

    ProjectList Documentation

--- src/Projects/ProjectList.java
+++ src/Projects/ProjectList.java
@@ -3,24 +3,52 @@ package Projects;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The ProjectList class represents a list of completed and unfinished projects
+ * It provides methods for adding, retrieving, and removing projects,
+ * as well as searching for projects by ID, type, and marking projects as finished
+ *
+ * @author
+ * @version 1.0 - December 2023
+ */
 public class ProjectList
 {
   private List<Project> completedProjects = new ArrayList<>();
   private List<Project> unfinishedProjects = new ArrayList<>();
 
+  /**
+   * Default constructor for creating a ProjectList with empty lists for completed and unfinished projects
+   *
+   */
   public ProjectList(){
     completedProjects=new ArrayList<>();
     unfinishedProjects=new ArrayList<>();
   }
 
+  /**
+   * Getter for the list of completed projects
+   *
+   * @return The list of completed projects
+   */
   public List<Project> getCompletedProjects() {
     return completedProjects;
   }
 
+  /**
+   * Getter for the list of unfinished projects
+   *
+   * @return The list of unfinished projects
+   */
   public List<Project> getUnfinishedProjects() {
     return unfinishedProjects;
   }
 
+  /**
+   * Adds a project to either the completed or unfinished projects list
+   *
+   * @param project    The project to add
+   * @param isFinished True if the project is finished, false otherwise
+   */
   public void addProject(Project project, boolean isFinished) {
     if (isFinished) {
       completedProjects.add(project);
@@ -29,6 +57,12 @@ public class ProjectList
     }
   }
 
+  /**
+   * Retrieves a project from either the completed or unfinished projects list based on the specified ID
+   *
+   * @param id The ID of the project to retrieve
+   * @return The project with the specified ID, or null if not found
+   */
   public Project retrieveProjectById(double id) {
     for (Project p : completedProjects) {
       if (p.getId() == id) {
@@ -45,6 +79,12 @@ public class ProjectList
     return null;
   }
 
+  /**
+   * Removes a project from either the completed or unfinished projects list
+   *
+   * @param project    The project to remove
+   * @param isFinished True if the project is finished, false otherwise
+   */
   public void removeProject(Project project, boolean isFinished) {
     if (isFinished) {
       completedProjects.remove(project);
@@ -53,6 +93,12 @@ public class ProjectList
     }
   }
 
+  /**
+   * Searches for a project with the specified type in either the completed or unfinished projects list
+   *
+   * @param projectType The type of the project
+   * @return The project with the specified type, or null if not found
+   */
   public Project searchProjectByType(String projectType) {
     for (Project p : completedProjects) {
       if (p.getProjectType().equals(projectType)) {
@@ -69,6 +115,11 @@ public class ProjectList
     return null;
   }
 
+  /**
+   * Marks a project as finished based on the specified project ID
+   *
+   * @param projectId The ID of the project to mark as finished
+   */
   public void markProjectAsFinished(double projectId) {
     Project projectToMark = null;
 
