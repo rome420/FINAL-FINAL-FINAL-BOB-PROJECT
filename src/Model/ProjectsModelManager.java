f4a888b720d6884dde781cab739bab8568a09633
rome420 <peeta2004@gmail.com>
2023-12-12 14:53:20 +0100

    Something

--- src/Model/ProjectsModelManager.java
+++ src/Model/ProjectsModelManager.java
@@ -1,5 +1,4 @@
-// ProjectsModelManager.java
-// ProjectsModelManager.java
+
 package Model;
 
 import java.util.ArrayList;
@@ -7,28 +6,37 @@ import java.util.List;
 
 // Import the Project class
 import Projects.Project;
+import Projects.ViewHandler;
+
+import javax.swing.text.View;
 
 public class ProjectsModelManager implements ProjectsModel {
   private List<Project> projects;
+  private ViewHandler viewHandler;
 
   public ProjectsModelManager() {
     this.projects = new ArrayList<>();
     // Initialize projects or load them from a data source if needed
   }
 
-  @Override
+
   public List<Project> getProjects() {
     return projects;
   }
 
-  @Override
   public void addProject(Project project) {
     projects.add(project);
   }
 
-  @Override
+
   public void removeProject(Project project) {
     projects.remove(project);
+  }
+
+
+  public void setViewHandler(ViewHandler viewHandler)
+  {
+    this.viewHandler = viewHandler;
   }
 
   // Implement other methods if needed
